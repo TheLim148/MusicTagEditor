fb168e24791d47489518263db85c89bdf60aefbb
TheLim148 <ruzhov34@gmail.com>
2025-07-21 17:44:22 +0300

    Added logs and some stuff with requests

--- JSONParse.py
+++ JSONParse.py
@@ -3,9 +3,78 @@ import acoustid
 import httpx
 import asyncio
 import socket
+
 import subprocess
+
+import logging
+
 import time
 import os
+
+API_KEY = "hv3SsHefpw"
+USER_API_KEY = "FnlllFTowF"
+
+JSON_TEMPLATE = '''
+            "albums": [
+                {
+                    "albumTitle": "",
+                    "date": ,
+                    "genres": [],
+                    "coverPath": "",
+                    "tracks": [
+                        {
+                            "title": "", "trackno": , "lenght": , "fingerprint": ""
+                        }
+                    ]
+                }
+            ]
+'''
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="debug_log.txt",
+    filemode="w",
+    format='[%(asctime)s] %(levelname)s - %(message)s'
+)
+
+# for score, recording_id, title, artist in acoustid.match(API_KEY, os.path.join("testFolder", "1.mp3")):
+#     print(f"\n{score}, {recording_id}, {title}, {artist}")
+
+# duration, fingerprint = acoustid.fingerprint_file("testFolder\\1​.mp3")
+
+def printData(dataFile):
+    with open(dataFile, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        
+    for artist in data["artists"]:
+        print(f"{artist["name"]}")
+        if "albums" in artist:
+            for album in artist["albums"]:
+                if "albumTitle" in album or "date" in album:
+                    print(f"|-{album["albumTitle"]}")
+                    print(f"|-{album["date"]}")
+                    for genre in album["genres"]:
+                        print(f"|---{genre}")
+                    for track in album["tracks"]:
+                        print(f"|---{track["trackno"]}, {track["title"]}, {track["length"]}")
+                else:
+                    print("|-(нету названия или даты)")
+        else:
+            print("|-(нету альбомов)")
+
+def writeToData(dataFile, foo):
+    with open(dataFile, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    # foo
+    # JSON_TEMPLATE
+
+    with open(dataFile, "w", encoding="utf-8") as f:
+        json.dump(data, foo, indent=2)
+
+def send_newnym():
+    with socket.create_connection(("127.0.0.1", 9051)) as s:
+        s.sendall(b'AUTHENTICATE \"\"\r\nSIGNAL NEWNYM\r\nQUIT\r\n')
 
 def isTorRunning(host="127.0.0.1", port=9050):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,6 +84,30 @@ def isTorRunning(host="127.0.0.1", port=9050):
             return True
         except (socket.timeout, ConnectionRefusedError):
             return False
+
+def clear_terminal():
+    subprocess.call(["cls"])
+
+def parse_track_info(data):
+    if "releases" not in data or not data["releases"]:
+        return {"error": "No releases found"}
+
+    results = []
+    
+    for release in data.get("releases", []):
+        for artist in release.get("artist_credit", []):
+            results.append({
+                "release_id": release.get("id", ""),
+                "album_title": release.get("title", ""),
+                "year": release.get("date".split("-", [0]), ""),
+                "artist_name": artist.get("name", ""),
+                "tags": release.get("tags", []),
+                "length": data.get("length", 0),
+                "song_title": data.get("title", "")
+            })
+    return results
+
+# def parse_album_info(data):
 
 async def get_musicbrainz_data(recording_id):
     url = f"https://musicbrainz.org/ws/2/recording/{recording_id}"
@@ -37,23 +130,14 @@ async def get_musicbrainz_data(recording_id):
             response = await client.get(url, params=params, headers=headers)
             response.raise_for_status()
             data = response.json()
-            results = []
-            for release in data.get("releases", []):
-                for artist in release.get("artist_credit", []):
-                    results.append({
-                        "release_id": release.get("id", ""),
-                        "album_title": release.get("title", ""),
-                        "year": release.get("date".split("-", [0]), ""),
-                        "artist_name": artist.get("name", ""),
-                        "tags": release.get("tags", []),
-                        "length": data.get("length", 0),
-                        "song_title": data.get("title", "")
-                    })
-            return results
+            return data
+            # parse_track_info(data)
         except httpx.HTTPStatusError as e:
             return {"error": "HTTP error", "details": repr(e)}
         except httpx.RequestError as e:
-            return {"error": "Request error", "details": repr(e)}
+            cause = e.__cause__
+            cause_msg = f"{type(cause).__name__}: {cause}" if cause else repr(e)
+            return {"error": "Request error", "details": cause_msg}
 
 
 async def get_track_number_in_release(release_id, recording_id):
@@ -92,59 +176,6 @@ async def get_track_number_in_release(release_id, recording_id):
         except httpx.RequestError as e:
             return {"error": "Request error", "details": repr(e)}
 
-API_KEY = "hv3SsHefpw"
-USER_API_KEY = "FnlllFTowF"
-
-JSON_TEMPLATE = '''
-            "albums": [
-                {
-                    "albumTitle": "",
-                    "date": ,
-                    "genres": [],
-                    "coverPath": "",
-                    "tracks": [
-                        {
-                            "title": "", "trackno": , "lenght": , "fingerprint": ""
-                        }
-                    ]
-                }
-            ]
-'''
-# for score, recording_id, title, artist in acoustid.match(API_KEY, os.path.join("testFolder", "1.mp3")):
-#     print(f"\n{score}, {recording_id}, {title}, {artist}")
-
-# duration, fingerprint = acoustid.fingerprint_file("testFolder\\1​.mp3")
-
-def printData(dataFile):
-    with open(dataFile, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        
-    for artist in data["artists"]:
-        print(f"{artist["name"]}")
-        if "albums" in artist:
-            for album in artist["albums"]:
-                if "albumTitle" in album or "date" in album:
-                    print(f"|-{album["albumTitle"]}")
-                    print(f"|-{album["date"]}")
-                    for genre in album["genres"]:
-                        print(f"|---{genre}")
-                    for track in album["tracks"]:
-                        print(f"|---{track["trackno"]}, {track["title"]}, {track["length"]}")
-                else:
-                    print("|-(нету названия или даты)")
-        else:
-            print("|-(нету альбомов)")
-
-def writeToData(dataFile, foo):
-    with open(dataFile, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    
-    # foo
-    # JSON_TEMPLATE
-
-    with open(dataFile, "w", encoding="utf-8") as f:
-        json.dump(data, foo, indent=2)
-
 async def main():
     match = next(acoustid.match(API_KEY, os.path.join("testFolder", "1.mp3")), "")
     if match:
@@ -152,11 +183,35 @@ async def main():
     else:
         raise ValueError("Совпадений не найдено")
     
-    result = await get_musicbrainz_data(recording_id)
-    if result is None:
+    max_attempts = 12
+    max_newnym_retries = 3
+
+    attempts = 0
+    newnym_attempts = 0
+    
+    while attempts < max_attempts:
+        result = await get_musicbrainz_data(recording_id)
+        if result and result.get("releases"):
+            break
+        
+        attempts += 1
+        print(f"Попытка {attempts}/{max_attempts} | {result}")
+        await asyncio.sleep(1)
+    
+        if attempts % 3 == 0:
+            newnym_attempts += 1
+
+            if newnym_attempts > max_newnym_retries:
+                raise ValueError("Не удалось получить данные после смены цепочки")
+            
+            send_newnym()
+            print("Перезапуск цепочки... подождите 3 секунды")
+            await asyncio.sleep(3)
+
+    if attempts == max_attempts:
         raise ValueError("Не удалось получить данные")
     
-    release_id = result[0]["release_id"] if result else ""
+    release_id = result[0]["release_id"]
     result1 = await get_track_number_in_release(release_id, recording_id)
 
     with open("trackInfo1.json", "w", encoding="utf-8") as f:
@@ -168,6 +223,7 @@ async def main():
     print(result)
 
 if __name__ == "__main__":
+    # clear_terminal()
     if isTorRunning():
         print("Tor is already runnig")
     else:
